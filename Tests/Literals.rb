8a910403f6ed53ab4178226d6a7a2438cd7681df
Joshua Hawxwell <m@hawx.me>
2010-10-27 19:33:29 +0100

    VERY experimental hashes

---
+++ Tests/Literals.rb
@@ -0,0 +1,30 @@
+'string'
+10448
+2142.51543
+:symbol
+:'stringbol'
+:"dstringbol"
+
+["an array", "of", 4, "items"]
+
+[
+  "an", [
+    "array", [
+      "in",
+    ]
+  ]
+  "an", [
+    "array", 
+  ]
+]
+
+hey = 5
+{
+  "key" => :value,
+  "what" => 10,
+  {
+    "what" => hey
+  }
+}
+
+[:a, :b, :c].each {|i| puts i } # shouldn’t be hash!
