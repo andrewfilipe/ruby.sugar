8bc1e0db452a633592ba6281477f5ed565d7df29
Joshua Hawxwell <m@hawx.me>
2010-11-22 17:34:47 +0000

    Added support for weird method names

---
+++ Tests/Methods.rb
@@ -0,0 +1,57 @@
+# This is a test for some crazy methods ruby has
+# in pairs, first with brackets second without
+
+def normal_method(arg1, arg2)
+end
+def normal_method arg1, arg2
+end
+
+def <<(val)
+end
+def << val
+end
+
+def <(val)
+end
+def < val
+end
+
+def <=>(val)
+end
+def <=> val
+end
+
+def [](val)
+end
+def [] val
+end
+
+def +(val)
+end
+def + val
+end
+
+def -(val)
+end
+def - val
+end
+
+def /(val)
+end
+def / val
+end
+
+def *(val)
+end
+def * val
+end
+
+def %(val)
+end
+def % val
+end
+
+def value=(val)
+end
+def value= val
+end
