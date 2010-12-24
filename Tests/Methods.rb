88dfb939864652b05f827132b49cd47df5f33dc2
Joshua Hawxwell <m@hawx.me>
2010-12-24 09:59:53 +0000

    Fixed method call highlighting problem

--- Tests/Methods.rb
+++ Tests/Methods.rb
@@ -55,3 +55,22 @@ def value=(val)
 end
 def value= val
 end
+
+# Method calls
+# ------------
+# Sometimes the arguments within brackets do not highlight as needed
+# which is obviously not what we want.
+# Turns out if you add "def xxx" the line before any function call it 
+# blanks out highlighting.
+
+my_method :sym, 123, "hello"
+my_method(:sym, 123, "hello")
+my_method(:sym, 123, "hello") {|arg| p arg }
+
+def method
+  what.my_method(:sym, 123, "hello")
+end
+
+def expires(amount, *values) # it's because of this line (comment out to see)
+  values << {} unless values.last.kind_of?(Hash) # here
+end
